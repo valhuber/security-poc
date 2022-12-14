c5b64d3797c4f18daf630f288392678e21d93c17
Val Huber <valjhuber@gmail.com>
2022-12-14 14:10:20 -0800

    authentication provider

--- security/security_sys.py
+++ security/security_sys.py
@@ -6,28 +6,18 @@ from sqlalchemy.orm import session
 from sqlalchemy import event, MetaData
 import safrs
 import database.models
-import security.authentication_provider.mem_auth as authentication_provider
+import security.authentication_provider.mem_auth as authentication_provider  # TODO: your provider here
+from sqlalchemy import event, MetaData
+from sqlalchemy.orm import with_loader_criteria
 
 print(f'\nsecurity_sys loaded via api_logic_server_run.py -- import \n')
 
 db = safrs.DB         # Use the safrs.DB, not db!
 session = db.session  # sqlalchemy.orm.scoping.scoped_session
 
-from sqlalchemy import event, MetaData
-from sqlalchemy.orm import with_loader_criteria
-
-
 def get_current_user():
-    """ stand-in for authorization """
+    """ STUB for authorization """
     return authentication_provider.Users.row("Client1")
-
-
-class Grants():  # nah
-    """
-    store grants, by entity
-    """
-    def __init__(self):
-        self.grants = {}  # entity, RolePermissions()
 
 
 class Grant:
@@ -51,55 +41,13 @@ class Grant:
         user_roles = user.roles
         mapper = orm_execute_state.bind_arguments['mapper']   # TODO table vs class (!!)
         table_name = mapper.persist_selectable.fullname   # mapper.mapped_table.fullname disparaged
-        grants_debug = Grant
         grants_for_class = Grant.grants_by_class[table_name]  # list of tuples: role, filter
 
         for each_grant_role, each_grant_filter in grants_for_class:
             if each_grant_role in user_roles:
-                print('Permission for user.role: {each_role}')
+                print(f'Execute Permission for class / role: {table_name} / {each_grant_role} - {each_grant_filter}')
                 orm_execute_state.statement = orm_execute_state.statement.options(
                     with_loader_criteria(database.models.Category, each_grant_filter))
-
-
-'''
-# **********************
-# stand-in for db users
-# **********************
-
-class User():
-    name = ""
-    roles = []
-
-    def __init__(self, name, roles):
-        self.name = name
-        self.roles = roles
-        Users.add(self)
-    
-    def __str__(self):
-        return(f'User[{self.name} has roles: {self.roles}')
-
-class Users:
-    users = {}
-
-    @staticmethod
-    def add(user):
-        Users.users[user.name] = user
-
-    def roles(self):
-        return self.roles
-    
-    @staticmethod
-    def row(name):
-        """ returns the user row """
-        return Users.users[name]
-
-
-sam = User("Sam", ("sa", "dev"))
-client1 = User("Client1", ("tenant", "manager"))
-client2 = User("Client2", ("rentor", "manager"))
-sam_row = Users.row("Sam")
-print(f"Sam: {sam}")
-'''
 
 
 @event.listens_for(session, 'do_orm_execute')
@@ -114,11 +62,9 @@ def receive_do_orm_execute(orm_execute_state):
         use_grant_class = True
         if use_grant_class:
             Grant.exec_grants(orm_execute_state)
-        else:
+        else:  # old code (disabled)
             mapper = orm_execute_state.bind_arguments['mapper']
             table = mapper.persist_selectable   # mapper.mapped_table.fullname disparaged
             if table.fullname == "Category":
                 orm_execute_state.statement = orm_execute_state.statement.options(
                     with_loader_criteria(database.models.Category, database.models.Category.Id == 1))
-    # print(f'boo ha')
-
