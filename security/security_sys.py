96d4ac52f7f04b82cb948f095924d324edaffa9f
Val Huber <valjhuber@gmail.com>
2022-12-16 16:24:34 -0800

    fix no grants for table

--- security/security_sys.py
+++ security/security_sys.py
@@ -35,6 +35,9 @@ class Security:
     @staticmethod
     @classmethod
     def current_user_has_role(role_name: str) -> bool: 
+        '''
+        If user has role xyz, then for update authorization he/she can... 
+        '''
         result = False
         for each_role in __class__.current_user.UserRoleList:
             if role_name == each_role.name:
@@ -44,39 +47,63 @@ class Security:
     
 
 class Grant:
-
-    """Invoke these to declare Role Permissions.
+    """
+    Invoke these to declare Role Permissions.
 
     Use code completion to discover models.
     """
 
     grants_by_class = {}
+    '''
+    Dict keyed by Table name, value is a (role,filter)
+    '''
 
     def __init__(self, on_entity: object, to_role: object = None, filter: object = None):
-        table_name = on_entity._s_class_name
-        if (table_name not in self.grants_by_class):
-            self.grants_by_class[table_name] = []
-        self.grants_by_class[table_name].append( (to_role, filter) )
+        '''
+        Create grant for <on_entity> / <to_role>
+
+        Example
+        =======
+        Grant(  on_entity = models.Category,  # use code completion
+                to_role = Roles.tenant,
+                filter = models.Category.Id == Security.current_user().client_id)  # User table attributes
+        
+        Args
+        ----
+            on_entity: a class from models.py
+            to_role: valid role name from Authentication Provider
+            filter: where clause to be added
+        
+        per calls from declare_security.py
+        '''
+        self.table_name = on_entity._s_class_name
+        self.role_name = to_role
+        self.filter = filter
+        if (self.table_name not in self.grants_by_class):
+            Grant.grants_by_class[self.table_name] = []
+        Grant.grants_by_class[self.table_name].append( self )
 
     @staticmethod
     def exec_grants(orm_execute_state):
+        '''
+        SQLAlchemy select event for current user's roles, append that role's grant filter to the SQL before execute 
+        '''
         user = Security.current_user()
-        user_roles = user.UserRoleList
         mapper = orm_execute_state.bind_arguments['mapper']   # TODO table vs class (!!)
         table_name = mapper.persist_selectable.fullname   # mapper.mapped_table.fullname disparaged
-        grants_for_class = Grant.grants_by_class[table_name]  # list of tuples: role, filter
-
-        for each_grant_role, each_grant_filter in grants_for_class:
-            for each_user_role in user.UserRoleList:
-                if each_grant_role == each_user_role.name:
-                    print(f'Execute Permission for class / role: {table_name} / {each_grant_role} - {each_grant_filter}')
-                    orm_execute_state.statement = orm_execute_state.statement.options(
-                        with_loader_criteria(database.models.Category, each_grant_filter))
+        if table_name in Grant.grants_by_class:
+            grants_for_class = Grant.grants_by_class[table_name]  # list of grants for this table
+            for each_grant in grants_for_class:
+                for each_user_role in user.UserRoleList:
+                    if each_grant.role_name == each_user_role.name:
+                        print(f'Execute Permission for class / role: {table_name} / {each_grant.role_name} - {each_grant.filter}')
+                        orm_execute_state.statement = orm_execute_state.statement.options(
+                            with_loader_criteria(database.models.Category, each_grant.filter))
 
 
 @event.listens_for(session, 'do_orm_execute')
 def receive_do_orm_execute(orm_execute_state):
-    "listen for the 'do_orm_execute' event"
+    "listen for the 'do_orm_execute' event from SQLAlchemy"
     if (
         orm_execute_state.is_select
         and not orm_execute_state.is_column_load
@@ -85,13 +112,13 @@ def receive_do_orm_execute(orm_execute_state):
         # print(f'receive_do_orm_execute alive')
         use_grant_class = True
         if use_grant_class:
-            mapper = orm_execute_state.bind_arguments['mapper']   # TODO table vs class (!!)
+            mapper = orm_execute_state.bind_arguments['mapper']   # TODO table vs class (!!case sensitive if names differ)
             table_name = mapper.persist_selectable.fullname   # mapper.mapped_table.fullname disparaged
             if table_name == "User":
                 pass  # TODO bypass authorization when rules are running
                 # print(f'avoid recursion on User table')
             else:
-                Grant.exec_grants(orm_execute_state)
+                Grant.exec_grants(orm_execute_state) # SQL read check grants
         else:  # old code (disabled)
             mapper = orm_execute_state.bind_arguments['mapper']
             table = mapper.persist_selectable   # mapper.mapped_table.fullname disparaged
